4a4925c0370d8a586667541ff7e89a0fefe9ff6d
mohamedslima <hammaslima@gmail.com>
2022-05-05 16:19:55 +0200

    print message in DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: "Guarda una bellissima foto del porto vecchio di Marsiglia",
+        }
+        
+    }
+    
+)
