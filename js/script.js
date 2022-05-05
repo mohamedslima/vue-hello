15ff814c012ad65653ea83695a986e3063d36e33
mohamedslima <hammaslima@gmail.com>
2022-05-05 16:50:33 +0200

    added styling to image and text

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ const app = new Vue(
         el: '#root',
         data: {
             message: "Guarda una bellissima foto del porto vecchio di Marsiglia",
+            imageMarseille: "https://franks-travelbox.com/wp-content/uploads/2017/11/frankreich-marseille-der-alte-hafen-geht-bis-auf-die-antike-zurucc88ck-und-gilt-als-touristisches-zentrum-von-marseille-frankreich-tatyana-vyc-shutterstock-1200x800.jpg",
         }
-        
     }
-    
-)
+);
+
+
+
